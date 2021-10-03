4d6a164d21d18672014a11a97b293e34c03d0bf6
wanghz18 <wanghz_thu18@163.com>
2021-10-03 23:28:26 +0800

    bank models

--- models/bank_rf.py
+++ models/bank_rf.py
@@ -1,11 +1,11 @@
-# bank LightGBM model
-# Test 0.9659824046920821
+# bank RF model
+# Test 0.9595307917888563
 # Train 1.0
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
 from imblearn.over_sampling import SMOTE
-from lightgbm import LGBMClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 random_state = 114
 data_table = pd.read_csv('data/bank.csv')
@@ -21,19 +21,12 @@ sm = SMOTE(random_state=random_state)
 X_train, y_train = sm.fit_resample(X_train, y_train)
 
 parameters = {
-        'n_estimators': 400,
-        'learning_rate': 0.25,
-        'num_leaves': 250,
-        'max_depth': 11,
-        'min_data_in_leaf': 320,
-        'lambda_l1': 0.65,
-        'lambda_l2': 1.08,
-        'bagging_fraction': 0.71,
-        'bagging_freq': 8,
-        'feature_fraction': 0.27,
+        'n_estimators': 150,
+        'max_depth': 30,
+        'random_state': random_state,
 }
 
-clf = LGBMClassifier(**parameters)
+clf = RandomForestClassifier(**parameters)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
